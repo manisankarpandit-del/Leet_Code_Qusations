4ad1a44c97970fba5881af0c68e31005a9e0497b
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-09-09 13:48:01 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (19.20%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long res = 0, p;
+        for (p = 1000; p <= n; p *= 1000)
+            res += (n - p + 1);
+        return res;
+    }
+};
